f0f1f8be891f29699cbc135d7a9d316b37c7d0da
Pablo Carranza Vélez <pablocarranza@gmail.com>
2013-08-13 03:40:43 -0300

    Now with a beatuifully horrendous one-liner

--- tabtospace.rb
+++ tabtospace.rb
@@ -1,7 +1,3 @@
 #!/usr/bin/env ruby
-if ARGV.length == 3
-	File.open(ARGV[1],"w"){|f| f.write(File.read(ARGV[0]).gsub("\t"," "*ARGV[2].to_i))}
-else
-	puts "Use: tabtospace.rb [infile] [outfile] [nspaces]"
-end 
+((ARGV.length == 3) && (File.open(ARGV[1],"w"){|f| f.write(File.read(ARGV[0]).gsub("\t"," "*ARGV[2].to_i))})) || puts "Use: tabtospace.rb [infile] [outfile] [nspaces]"
 
